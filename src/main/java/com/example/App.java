7d6bf7c863f654e77c639d177cc6f8ca5ed09980
Dominic Page <dominic.page@redis.com>
2023-04-04 18:15:58 +0200

    add retry on failed connection

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.Properties;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
@@ -19,7 +19,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+        try (InputStream input = new FileInputStream("jedis-demo/src/main/resources/config.properties")) {
 
             Properties prop = new Properties();
             prop.load(input);
@@ -32,48 +32,58 @@ public class App
 
     private static void managePool(Properties prop) {
         GenericObjectPoolConfig<Jedis> poolConfig = new GenericObjectPoolConfig<>();
-        // underdimension to force some queuing behaviour
         poolConfig.setMaxTotal(Integer.parseInt(prop.getProperty("pc.maxtotal")));
-        //poolConfig.setMaxIdle(Integer.parseInt(prop.getProperty("ts.max")) / 2);
-        //poolConfig.setMinIdle(Integer.parseInt(prop.getProperty("pc.minidle")));
         
+        System.out.printf(
+            "Target => %s:%s\n", 
+            System.getenv("RS_HOST"),
+            System.getenv("RS_PORT"));
+
         JedisPool pool = new JedisPool(
             poolConfig, 
-            prop.getProperty("rs.host"),
-            Integer.parseInt(prop.getProperty("rs.port")));
-        String auth = prop.getProperty("rs.auth");
+            System.getenv("RS_HOST"),
+            Integer.parseInt(System.getenv("RS_PORT")));
+        pool.setMaxWait(Duration.ofSeconds(10));
+        String auth = System.getenv("RS_AUTH");
 
-        ArrayList<Jedis> resources = new ArrayList<Jedis>();
-        for (int i = 0; i < Integer.parseInt(prop.getProperty("ts.max")); i++ ) {
-            // don't trust in try to close as threads getting crossed up 
-            Thread hwThread = new Thread(() -> { 
-                helloWorld(pool.getResource(), auth);
-            });
-            hwThread.start();
-            //resourceInfo(prop, pool);
+        int i = 0; 
+        int f = 0;
+        while (i < Integer.parseInt(prop.getProperty("ts.max"))) {
+            // don't trust in try with resources to close as last two threads were getting crossed up?
+            try {
+                Jedis j = pool.getResource();
+                Thread hwThread = new Thread(() -> { 
+                    helloWorld(j, auth);
+                });
+                hwThread.start();
+                i++;
+            } catch (redis.clients.jedis.exceptions.JedisConnectionException e) {
+                System.out.printf("Failed to connect to any host resolved for DNS name :(");
+                f++;
+            }
         }
         
         Jedis jedis = pool.getResource();
         
         jedis.auth(auth);
         if (pingWait(jedis)) {
-            int count = safeGetInt(jedis, "count");
+            int count = safeGetHInt(jedis, "jedis", "count");
             while (count < Integer.parseInt(prop.getProperty("ts.max"))) { 
                 System.out.printf("parent> GET %s => %s\n", "count", count);
                 resourceInfo(prop, pool);
                 waitms(100);
-                count = safeGetInt(jedis, "count");
+                count = safeGetHInt(jedis, "jedis", "count");
             }
         }
 
-        System.out.printf("parent> DEL count => %s\n", jedis.del("count"));
-
+        System.out.printf("parent> DEL jedis => %s\n", jedis.del("jedis"));
         pool.close();
+        System.out.printf("Threads launched => %d\nFail/Retry total => %d\n", i, f);
     }
 
-    private static int safeGetInt(Jedis jedis, String key) {
-        String v = jedis.get(key);
-        System.out.printf("safeGetInt(%s) => %s\n", key, v);
+    private static int safeGetHInt(Jedis jedis, String key, String field) {
+        String v = jedis.hget(key, field);
+        System.out.printf("safeGetHInt(%s, %s) => %s\n", key, field, v);
         return v != null ? Integer.parseInt(v) : 0;
     }
 
@@ -82,15 +92,15 @@ public class App
        
         jedis.auth(auth);
         if (pingWait(jedis)) {
-            //String k = "hello";
-            String k = t;
-            String v = "world";
+            String k = "jedis";
+            String v = "test";
+            String c = "count";
             //System.out.printf("%s> INCR lock => %s\n", t, jedis.incr("lock"));
-            System.out.printf("%s> SET %s %s => %s\n", t, k, v, jedis.set(k, v));
-            System.out.printf("%s> GET %s => %s\n", t, k, jedis.get(k));
+            System.out.printf("%s> HSET %s %s %s => %s\n", t, k, t, v, jedis.hset(k, t, v));
+            System.out.printf("%s> HGET %s %s => %s\n", t, k, t, jedis.hget(k, t));
             //System.out.printf("%s> INCRBY lock -1 => %s\n", t, jedis.incrBy("lock", -1));
             // nope, the lock can transition through 0 without completing all threads
-            System.out.printf("%s> INCR count => %s\n", t, jedis.incr("count"));
+            System.out.printf("%s> INCR count => %s\n", t, jedis.hincrBy(k, c, 1));
         }
         jedis.close();
     }
