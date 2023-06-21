682e11cacd9659e849435b140a6ac10e87dc22c3
Dominic Page <dominic.page@redis.com>
2023-06-21 12:54:29 +0200

    你好世界

--- src/main/java/com/example/BytesDemo.java
+++ src/main/java/com/example/BytesDemo.java
@@ -6,7 +6,7 @@ public class BytesDemo {
     public static void main(String[] args) {
         Jedis jedis = new Jedis();
         String k = "ORIGINAL";
-        String v = "豐盛戶售後服務-提供市場訊息";
+        String v = "你好世界";
         System.out.printf("> SET %s %s\n%s\n", k, v, jedis.set(k, v));
         System.out.printf("> GET %s\n%s\n", k, jedis.get(k));
         jedis.close();
